f6cc5c15dbc6ed13d16b93e2d913156b9b1072af
JakobSundstrom <jakob.sundstrom1@gmail.com>
2023-07-31 15:47:12 +0200

    create working zoom function

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Function to handle the scrollbar click event
-function handleScrollbarClick(event) {
-    // Get the viewport element
-    const viewport = document.getElementById("viewport");
-
-    // Get the click position relative to the scrollbar
-    const clickPosition = event.clientY - event.currentTarget.getBoundingClientRect().top;
-
-    // Calculate the scroll position based on the click position inside the scrollbar
-    const scrollHeight = viewport.scrollHeight - viewport.clientHeight;
-    const scrollTo = (clickPosition / event.currentTarget.clientHeight) * scrollHeight;
-
-    // Scroll the viewport to the calculated position
-    viewport.scrollTo({ top: scrollTo, behavior: "smooth" });
-}
+function changeSize() {
+    var scroll = window.scrollY;
+    var width = Math.min(100, scroll / 5); // Maximum width of 100%
+    var height = Math.min(400, scroll); // Maximum height of 400 pixels
+  
+    document.getElementById('expand').style.width = width + '%';
+    document.getElementById('expand').style.height = height + 'px';
+  }
+  
+  window.addEventListener('scroll', function() {
+    requestAnimationFrame(changeSize);
+  });
+  
